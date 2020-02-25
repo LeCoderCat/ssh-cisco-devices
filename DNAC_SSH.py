96fff295c8f1a0589840fcdd3fa6998cd15d086a
LeCoderCat <61458964+LeCoderCat@users.noreply.github.com>
2020-02-25 17:18:20 +0100

    Update DNAC_SSH.py
    Corrected errors

--- DNAC_SSH.py
+++ DNAC_SSH.py
@@ -7,27 +7,21 @@ host = 'IP'
 username = 'maglev' 
 port = 2222
 password = 'PASSWORD'
-command = 'maglev package status \n'
-command1 = 'GUI_USERNAME \n GUI_PASSOWORD\n'
-#command1 = 'magctl appstack status \n'
+command = 'magctl appstack status \n'
 
 try:
-    client = paramiko.SSHClient()
-        #client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        #client.set_missing_host_key_policy(paramiko.WarningPolicy)
-    client.connect(hostname=host, username=username, password=password, port=port)
-    print('Logged in DNAC. . .\n')
-    stdin, stdout, stderr = client.exec_command(command, get_pty=True)
-    stdin.write(command1)
-    stdin.flush()
-    for line in stdout:
-        print (line.strip('\n'))
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=username, password=password, port=port)
+    print('Logged in DNAC. . .\n')
+    stdin, stdout, stderr = client.exec_command(command, get_pty=True)
+    stdin.flush()
+    for line in stdout:
+        print (line.strip('\n'))
 
-except SSHException:
-    client.close()
+except SSHException:
+    client.close()
 
 finally:
-    client.close()
-    print ('Connection closed. . .\n')
-
+    client.close()
+    print ('Connection closed. . .\n')
