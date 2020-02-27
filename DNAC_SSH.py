f869ad0443f85dfe8c1ec5eae60468e1ded9825d
LeCoderCat <61458964+LeCoderCat@users.noreply.github.com>
2020-02-27 12:16:33 +0100

    Update DNAC_SSH.py
    Python 3

--- DNAC_SSH.py
+++ DNAC_SSH.py
@@ -8,19 +8,16 @@ port = 2222
 password = 'PASSWORD'
 command = 'magctl appstack status \n'
 
-try:
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=username, password=password, port=port)
-    print('Logged in DNAC. . .\n')
-    stdin, stdout, stderr = client.exec_command(command, get_pty=True)
-    stdin.flush()
-    for line in stdout:
-        print (line.strip('\n'))
+command = 'magctl appstack status \n'
+command1 = 'logout \n'
 
-except SSHException:
-    client.close()
-
-finally:
-    client.close()
-    print ('Connection closed. . .\n')
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=host, username=username, password=password, port=port)
+print('Logged in DNAC. . .\n')
+stdin, stdout, stderr = client.exec_command(command, get_pty=True)
+stdin.write(command1)
+stdin.flush()
+for line in stdout:
+    print (line.strip('\n'))
+#client.close()
